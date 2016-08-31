60acff2952c3705be3eb1625e5dda507b2a82cf0
Ivica Nikolov <inikolovbiz@gmail.com>
2016-08-31 10:43:18 +0200

    Ang project first commit

---
+++ app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html'
+})
+
+export class AppComponent {
+    title: string;
+    timesClicked: number = 0;
+    inputtext = 'This is default text';
+    anothertext = 'Another default text';
+    messages: string [] = [];
+    constructor() {
+        this.title = 'Angular 2 Binding Events'
+    }
+    log(msg: string, data: string){        
+        this.timesClicked+=1;
+        this.messages.splice(0,0, msg + " " + this.timesClicked);
+        console.log(msg);
+        if(data){
+        }
+    }
+}
