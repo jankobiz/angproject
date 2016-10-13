2e66a8ce947ff7a6288cabf535bf1939b59952f6
jankobiz <inikolovbiz@gmail.com>
2016-10-13 09:33:36 +0200

    Home 2 branch first commit

--- app/app.custom-pipe.ts
+++ app/app.custom-pipe.ts
@@ -9,11 +9,12 @@ export class MyPipe implements PipeTransform {
     transform (value: Vehicle[], args: string[]): Vehicle[] {        
         let filter: string = args[0] ? args[0].toLocaleLowerCase() : null;
         let transformed: Vehicle[] = [  
-  {id: 2, brand: 'Mazda', model: "636 TDI", color: "blue", price: 24000},    
-  {id: 5, brand: 'Kia', model: "Sephia 2", color: "silver", price: 19600},    
-  {id: 8, brand: 'Seat', model: "Ibiza 16v", color: "magenta", price: 24500},
-  {id: 9, brand: 'Zastava', model: "Yugo 55", color: "yellow", price: 20400}  
-];        
+            {id: 2, brand: 'Mazda', model: "636 TDI", color: "blue", price: 24000},    
+            {id: 5, brand: 'Kia', model: "Sephia 2", color: "silver", price: 19600},    
+            {id: 8, brand: 'Seat', model: "Ibiza 16v", color: "magenta", price: 24500},
+            {id: 9, brand: 'Zastava', model: "Yugo 55", color: "yellow", price: 20400},
+            {id: 9, brand: 'Zastava automobili', model: "Skala 55", color: "yellow", price: 20400}
+        ];        
         if (args[0]) {
             console.log(args.toString());
             console.log(args[0].toLocaleLowerCase());
