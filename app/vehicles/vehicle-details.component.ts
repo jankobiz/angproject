6eaa58bc57e5c606c51648083e19a42e8a910381
Ivica Nikolov <inikolovbiz@gmail.com>
2016-12-23 18:56:48 +0100

    Adding vehicle detail component

---
+++ app/vehicles/vehicle-details.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, Output, EventEmitter, OnChanges, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { VehicleService } from './vehicle-service';
+
+import { Vehicle } from './vehicle';
+
+@Component ({
+    selector: 'vehicle-details',        
+    templateUrl: 'app/vehicles/vehiclel-detail.component.html'      
+})
+
+export class VehicleDetail {
+    
+}
